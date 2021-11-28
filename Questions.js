da48d2f67a9a79b57569ad16ff51fc0c3552b4e2
Christian-Henriquez <chris-henriquez@outlook.comgit config --global user.name Christian-Henriquez>
2021-11-27 19:39:51 -0700

    Added new quiz questions

--- Questions.js
+++ Questions.js
@@ -1,85 +1,35 @@
 var questions = [
     {
-        title: "Which one is a looping structure in JavaScript?",
-        choices: ["All the below", "For", "While", "do-while loops"],
-        answer: "All the below"
+        title: "When was the constitution signed?",
+        choices: ["1812", "1900", "1776", "1475"],
+        answer: "1776"
     },
     {
-        title: "What are the two basic groups of data types in JavaScript?",
+        title: "When was the war of 1812?",
         choices: [
-            "Primitive and attribute",
-            "Primitive and reference types",
-            "Reference types and attribute",
+            "1776",
+            "1996",
+            "1812",
             "None of the above"
         ],
-        answer: "Primitive and reference types"
+        answer: "1812"
     },
     {
-        title: "Commonly used data types DO NOT include:",
-        choices: ["strings", "booleans", "alerts", "numbers"],
-        answer: "alerts"
-    },
-    {
-        title: "Boolean operators that can be used in JavaScript include:",
+        title: "Gutzon Borglum began carving which giant iconic sculpture in 1927?",
         choices: [
-            "'And' Operator &&",
-            "'Or' Operator ||",
-            "'Not' Operator !",
-            "All the above"
-        ],
-        answer: "All the above"
+        "Statue of Liberty", 
+        "Statue of David",
+        "Mount Rushmore",
+
+        answer: "Mount Rushmore"
     },
     {
-        title:
-            "Which one of these is not among the three different types of errors in JavaScript?",
+        title: "Which official motto of the United States first appeared on paper currency in October 1957?"
         choices: [
-            "Animation time errors",
-            "Load time errors",
-            "Run time errors",
-            "Logical Errors"
+            "E pluribus unum",
+            "New World Order",
+            "In God We trust",
+        
         ],
-        answer: "Animation time errors"
+        answer: "In God We Trust"
     },
-    {
-        title: "What is the data type of variables in JavaScript?",
-        choices: [
-            "Object data types",
-            "Function data type",
-            "None of the above",
-            "All of the above"
-        ],
-        answer: "Object data types"
-    },
-    {
-        title: "The condition in an if / else statement is enclosed within ____.",
-        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-        answer: "parentheses"
-    },
-    {
-        title: "Arrays in JavaScript can be used to store ____.",
-        choices: [
-            "numbers and strings",
-            "other arrays",
-            "booleans",
-            "all of the above"
-        ],
-        answer: "all of the above"
-    },
-    {
-        title:
-            "String values must be enclosed within ____ when being assigned to variables.",
-        choices: ["commas", "curly brackets", "quotes", "parentheses"],
-        answer: "quotes"
-    },
-    {
-        title:
-            "A very useful tool used during development and debugging for printing content to the debugger is:",
-        choices: ["JavaScript", "terminal / bash", "for loops", "console.log"],
-        answer: "console.log"
-    },
-    {
-        title: "What is the type of Pop up boxes available in JavaScript?:",
-        choices: ["Alert", "Confirm", "Prompt", "All the above"],
-        answer: "All the above"
-    }
-];
