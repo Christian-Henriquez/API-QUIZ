c7061a2eeed5e0fb5b7400300d1258ee767e6581
Christian-Henriquez <chris-henriquez@outlook.comgit config --global user.name Christian-Henriquez>
2021-11-30 23:52:07 -0700

    modifiied script

--- Scripts.js
+++ Scripts.js
@@ -157,5 +157,7 @@ function clearHighscores() {
     window.localStorage.removeItem("highscores");
     window.location.reload();
 }
+{
 document.getElementById("clear").onclick = clearHighscores;
 printHighscores();
+}
